cf407524cecd9b04429e60e45726c6b08f695bd8
gabxt <67200783+gabxt@users.noreply.github.com>
2023-10-12 12:18:35 +0200

    Update kapcs.php

--- kapcs.php
+++ kapcs.php
@@ -1,5 +1,5 @@
 <?php
-$kapcsolat = mysql_connect("127.0.0.1", "gabxway", "Hall2000-");
+$kapcsolat = mysql_connect("127.0.0.1", "", "");
 if (!$kapcsolat) die("Nem sikerült kapcsolódni az adatbázishoz!");
 mysql_select_db("gabxway", $kapcsolat) or die("Nem sikerült kiválasztani az adatbázist!");
 mysql_close($kapcsolat);
