6739dc7cdc1945967c72658e820e4dbbae2c4ecd
gabxt <67200783+gabxt@users.noreply.github.com>
2023-10-12 12:21:19 +0200

    Update chat_con.php

--- chat_con.php
+++ chat_con.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "gabxway";
-$password = "Hall2000-";
-$dbname = "gabxway";
+$username = "";
+$password = "";
+$dbname = "";
 
 // Kapcsolódás az adatbázishoz
 $conn = new mysqli($servername, $username, $password, $dbname);
